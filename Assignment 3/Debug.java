17a6b048cd7dfe12081fdc16b64926468b74e36b
CalebK144 <calebk.nov14@gmail.com>
2025-09-19 16:54:19 -0400

    Update Debug.java

--- Assignment 3/Debug.java
+++ Assignment 3/Debug.java
@@ -4,7 +4,7 @@ public class Main {
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
         System.out.println("Problem 1");
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i =< 10; i++){
             System.out.println(i);
         }
 
@@ -12,8 +12,13 @@ public class Main {
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
         System.out.println("Enter a number and I will tell you the factorial: ");
-        //here's a hint
-        for (int i = 0; i < ?; i++){
+        int num = input.nextInt();
+        int factorial = 1;
+        for (int i = 0; i < num; i++) {
+            factorial *= i;
+        }
+        System.out.println(num + "! = " + factorial);
+
 
         }
 
@@ -21,6 +26,12 @@ public class Main {
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
         //No hint! what do you need to complete this task?
+        int limit = input.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= limit; i += 2) {
+            sum += i;
+        }
+        System.out.println("The sum of every other number up to " + limit + " is " + sum)
 
 
         //P4: Why does this loop never stop!
@@ -29,12 +40,21 @@ public class Main {
         boolean run = true;
         while (run == true){
             System.out.println("I printed once!");
+            run = false;
         }
 
         //P5: Take a string from the user and print them the reverse!
         System.out.println("Problem 5");
         //hint
         String reverse = "";
+        System.out.println("Enter a string");
+        String inputString = sc.next();
+        int bound = inputString.length() - 1
+        for (int i = bound; i >= 0; i--) {
+            reverse += inputString.charAt(i);
+        }
+        System.out.println(reverse);
+            
 
     }
 }
